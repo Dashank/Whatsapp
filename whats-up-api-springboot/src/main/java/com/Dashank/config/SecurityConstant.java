74ff0dae585271f84cdc8ad216ffbf75278a0503
Dashank <dashankbhoir60@gmail.com>
2023-06-13 12:11:30 +0530

    whatsapp web application
    frontend and backend commit

--- whats-up-api-springboot/src/main/java/com/Dashank/config/SecurityConstant.java
+++
@@ -1,9 +0,0 @@
-package com.Dashank.config;
-
-public class SecurityConstant {
-	
-	public static final String JWT_KEY="kzjjbeiurbZGyurZvzpaqekmeecfeeljliuogcerwmqzsduphbeheb";
-	public static final String HEADER = "Authorization";
-
-
-}
